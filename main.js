bd01a97e032c4983f0b12e9494730d7cfc09d246
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-07-28 21:53:30 -0400

    unfinished

--- main.js
+++ main.js
@@ -1,6 +1,66 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const newPerson = function(firstName, lastName, age, married = false){
+  if(firstName === undefined){
+    firstName = 'Anonymous'
+  }else if(lastName === undefined){
+    lastName = 'Person'
+  }
+
+  
+  
+  const person = {
+
+    firstName,
+    lastName,
+    age,
+    married,
+
+    goingOn:  function(){
+      return this.age + 1
+    },
+
+    ageUp: function(){
+      person.age++
+    },
+
+    getFullName: function(){
+      return this.firstName + ' ' + this.lastName
+    },
+
+    marry: function(suitor){
+      this.married = true;
+      suitor.married = true;
+      this.spouseName = suitor.firstName + ' ' + suitor.lastName;
+      suitor.spouseName = this.firstName + ' ' + this.lastName
+    },
+
+    divorce: function(suitor){
+      this.married = false;
+      suitor.married = false;
+      
+
+      
+    }
+    
+
+
+  };
+
+
+ 
+  
+
+
+
+
+  return person
+}
+
+
+
+
 
 
 
